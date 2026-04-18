319d9f3b1e40acf24397b5d923fc42b7c66855f6
Alex C <cooka5@tcnj.edu>
2026-04-18 13:08:07 -0400

    Mobster & Scrappybara Animations

--- Assets/Game Assets/Scripts/ScrappyScript.cs
+++ Assets/Game Assets/Scripts/ScrappyScript.cs
@@ -9,11 +9,12 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-public class ScrappyScript : MonoBehaviour
-{
+public class ScrappyScript : MonoBehaviour {
     [SerializeField] GameObject playerObject; 
     [SerializeField] movement playerScript; 
     Rigidbody rb;
+    Animator animator;
+    
     float speed = 3f;
     Vector3 movement; 
     float distance;
@@ -21,35 +22,35 @@ public class ScrappyScript : MonoBehaviour
     float slowDownSpeed = 5f; 
     float maxDistance = 10f;
     public int health = 3; 
-    bool canPunch = false; 
-    float counter = 1; 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    bool canPunch = true; 
+    float counter = 1;
+
+
+    void Start() {
         rb = this.gameObject.GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();  // Get the component from the model's GameObject
     }
-    // Update is called once per frame
-    void Update()
-    {
+
+    void Update() {
         Vector3 target = playerObject.transform.position; 
         Vector3 currentLocal = this.transform.position; 
         distance = Vector3.Distance(target,currentLocal);
 
-        if (distance < maxDistance)
-        {
+        if (distance < maxDistance && canPunch) {
             canMove = true; //so they start only after a certain distance
+            animator.SetBool("walking", true);  // Play Animation
+        } else {
+            animator.SetBool("walking", false);
         }
 
-        if (canMove && distance > 2 && health > 0)
-        {
+        if (canMove && distance > 2 && health > 0) {
             movement = new Vector3(transform.forward.x, 0, transform.forward.z);
             movement *= speed;  
             movement = Vector3.ClampMagnitude(movement, speed); 
             movement.y = rb.linearVelocity.y;
 
             rb.linearVelocity = movement; 
-        } else
-        {
+        } else {
             movement.x = Mathf.Lerp(rb.linearVelocity.x, 0, Time.deltaTime * slowDownSpeed);
             movement.z = Mathf.Lerp(rb.linearVelocity.z, 0, Time.deltaTime * slowDownSpeed); 
             movement.y = rb.linearVelocity.y;
@@ -59,75 +60,78 @@ public class ScrappyScript : MonoBehaviour
 
         Vector3 look = new Vector3(playerObject.transform.position.x, this.transform.position.y, playerObject.transform.position.z);
         
-        if (health > 0 && canMove)
-        {
+        if (health > 0 && canMove) {
             transform.LookAt(look);
         }
 
         RaycastHit hit; 
-//Punch Mechanic
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 2.5f)) //2.5 is maxdistance 
-        {
+        //Punch Mechanic
+        if ( canPunch && Physics.Raycast(transform.position, transform.forward, out hit, 2.5f) ) { //2.5 is maxdistance 
+            if (hit.transform.gameObject != null) {  // If object is in the way
+                canPunch = false;  // Apply punch cooldown
+                canMove = false;  // Stop the enemy from moving
+                animator.SetTrigger("Attack");  // Play Animation
+                string tag = hit.transform.gameObject.tag;  // Get tag of obstacle
 
-            if (hit.transform.gameObject != null)
-            {
-                string tag = hit.transform.gameObject.tag; 
-                if (tag == "Player")
-                {
-                    counter += Time.deltaTime; 
-                    if (counter >= 2)
-                    {
-                        StartCoroutine(punch());
-                        if (canPunch)
-                        {
-                            playerScript.gotBread(-1);
-                            Debug.Log("Punch!");
-                            counter = 0f; 
-                            canPunch = false; 
+                switch (tag) {
+                    case "Player":
+                        counter += Time.deltaTime; 
+                        if (counter >= 2) {
+                            StartCoroutine(punch());  // Attempt to hit the player with a punch
+                        } else {
+                            canMove = true;
+                            canPunch = true;  // Allow the Scrappybara to punch again.
                         }
-                    } 
-                } else if (tag == "crate")
-                {
-                    counter += Time.deltaTime; 
-                    if (counter >= 2f)
-                    {
-                        BreakCrateScript crate = hit.transform.gameObject.GetComponent<BreakCrateScript>(); 
-                        if (canPunch)
-                        {
+                        break;
+                    case "crate":
+                        counter += Time.deltaTime; 
+                        if (counter >= 2f) {
+                            BreakCrateScript crate = hit.transform.gameObject.GetComponent<BreakCrateScript>(); 
                             counter = 0f;
                             crate.crateHit();
-                            canPunch = false; 
                         }
-                    }
-                } else 
-                {
-                    counter = 0.1f;
+                        StartCoroutine(punch());
+                        break;
+                    default:
+                        StartCoroutine(punch());
+                        counter = 0.1f;
+                        break;
                 }
-            } else
-            {
-                counter = 0f; 
+            } else {
+                canPunch = true;  // Allow the Scrappybara to punch again.
+                counter = 0f;
             }
         }
-
     }
 
-    IEnumerator punch()
-    {
-        canMove = false; 
+    IEnumerator punch() {  // Attempt to hit the player with a punch
+        
         Debug.Log("Not Moving");
-        yield return new WaitForSeconds(2); 
-        canPunch = true; 
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.8f);
+        
+        // Test if punch landed.
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 2.5f) ) { //2.5 is maxdistance 
+            if (hit.transform.gameObject != null) {  // If object is in the way
+                string tag = hit.transform.gameObject.tag;  // Get tag of obstacle
+                if (tag == "Player") {
+                    playerScript.gotBread(-1);
+                    Debug.Log("Took Damage!");
+                }
+            }
+        }
+        yield return new WaitForSeconds(1f);
         canMove = true;
-
+        canPunch = true;  // Allow the Scrappybara to punch again.
     }
     
 
-    public void gotShot()
-    {
+    public void gotShot() {
+        animator.SetTrigger("Hit");  // Play Animation
         health -= 1; 
         if (health <= 0)
         {
+            animator.SetTrigger("Die");  // Play Animation
             Destroy(this.gameObject);
         }
     }
